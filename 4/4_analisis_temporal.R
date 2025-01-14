7ecb1bd3f345aee90034104c25e291c05679d1e4
javicarela <66629778+javicarela@users.noreply.github.com>
2025-01-14 10:00:52 +0100

    Update 4_analisis_temporal.R

--- 4/4_analisis_temporal.R
+++ 4/4_analisis_temporal.R
@@ -1,7 +1,7 @@
 #Area verde
 # El area everde de la predicción son aquellos valores en los que las casas de apuestas
 # valoran entre 1.6 y 2.4 de cuota. ¿Porque ? porque son los que peores se predicen
-load("~/prebet/df.RData")
+load("~/prebet/data/frames/df.RData")
 library(data.table)
 library(knitr)
 library(caret)
