8a075b5825cd1707df6d1b7a4c2312d3611f9b25
javicarela <66629778+javicarela@users.noreply.github.com>
2025-01-17 09:33:52 +0100

    Update 2.R

--- 2/2.R
+++ 2/2.R
@@ -1,4 +1,4 @@
-# Exploración y modificación para la parte 4
+# Exploration and modification for 4 part
 load("~prebet/data_frames/NBA_games_info.RData")
 df$Date <- as.Date(df$Date)
 df <- df %>%
@@ -40,11 +40,11 @@ df <- df %>%
   mutate(score_tmp = ifelse(Points > OpponentPoints, 1, -1)) %>%
   # Agrupar por Team y Opponent para considerar solo partidos entre ellos
   group_by(Team, Opponent) %>%
-  # Calcular el puntaje acumulado de los últimos 10 partidos entre Team y Opponent, excluyendo el actual
+  # Points acumulated in the last 10 games between  Team and Opponent, extracting the actual game
   mutate(score_last_5_between = lag(rollapply(score_tmp, width = 5, align = "right", 
                                                FUN = sum, fill = NA, partial = TRUE), default = NA)) %>%
   ungroup() %>%
-  # Eliminar la columna temporal
+  # Delete the temporal column
   select(-score_tmp)
 
 df$hour <- (df$hour - min(df$hour)) / (max(df$hour) - min(df$hour))
